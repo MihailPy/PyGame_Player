269a35a9b3ed7b8588b7a69c7947c6ec67883303
MihailPy <43038605+MihailPy@users.noreply.github.com>
2018-10-17 19:39:47 +0300

    Add files via upload

--- geme_objects.py
+++ geme_objects.py
@@ -1,8 +1,10 @@
-import random
-
+import random, time
 import pygame
 
 from settings import WIDTH, HEIGHT
+
+#class Button(pygame.sprite.Sprite):
+
 
 class Plasmoid(pygame.sprite.Sprite):
     speed = -15
@@ -27,7 +29,11 @@ class Player(pygame.sprite.Sprite):
         self.clock = clock
         self.plasmoids = plasmoids
 
-        self.image = pygame.image.load("assets/Player.png")
+
+        self.texture = pygame.image.load("assets/PlayerA.png")
+        #spritesheet = pygame.image.load("assets/PlayerA.png")
+
+        self.image = self.texture.subsurface(pygame.Rect(899, 110, 74, 74))
         self.rect = self.image.get_rect()
 
         self.rect.centerx = WIDTH / 2
@@ -37,20 +43,77 @@ class Player(pygame.sprite.Sprite):
 
         self.current_shooting_cooldown = 0
 
+        self.flip = []
+        self.flip.append(self.texture.subsurface(pygame.Rect(814, 109, 74, 74)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(733, 109, 74, 76)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(655, 109, 73, 77)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(576, 104, 69, 79)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(501, 102, 64, 81)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(444, 99, 54, 85)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(402, 96, 33, 88)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(369, 94, 22, 91)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(343, 95, 11, 91)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(302, 95, 28, 89)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(243, 98, 42, 87)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(176, 104, 58, 81)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(98, 107, 67, 67)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(16, 110, 73, 73)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(895, 22, 71, 74)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(812, 20, 73, 71)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(737, 18, 67, 76)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(673, 13, 57, 80)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(919, 10, 44, 85)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(579, 4, 23, 89)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(547, 3, 12, 90)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(515, 4, 21, 90)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(464, 7, 39, 87)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(404, 9, 54, 84)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(335, 11, 64, 82)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(260, 14, 71, 80)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(182, 16, 74, 77)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(104, 18, 74, 75)))
+        self.flip.append(self.texture.subsurface(pygame.Rect(14, 20, 75, 73)))
+        self.f = 0
     def update(self):
+        def flipR():
+            print (self.f)
+            self.current_speed = +self.max_speed - 5
+            self.image = self.flip[self.f]
+            if self.f == len(self.flip) - 1:
+                self.f = 0
+            else:
+                self.f += 1
         if self.rect.centerx > 365:
             self.rect.centerx = 367
         elif self.rect.centerx < 30:
             self.rect.centerx = 32
+        def flipL():
+            print(len(self.flip))
+            print (self.f)
+            self.current_speed = -self.max_speed + 5
+            self.image = self.flip[self.f]
+            if self.f == -29:
+                self.f = 0
+            else:
+                self.f -= 1
 
         keys = pygame.key.get_pressed()
 
+
         if keys[pygame.K_LEFT]:
             self.current_speed = -self.max_speed
+            self.image = self.texture.subsurface(pygame.Rect(103, 20, 75, 75))
         elif keys[pygame.K_RIGHT]:
             self.current_speed = +self.max_speed
+            self.image = self.texture.subsurface(pygame.Rect(735, 110, 75, 75))
+        elif keys[pygame.K_d]:
+            flipR()
+        elif keys[pygame.K_a]:
+            flipL()
         else:
+            self.image = self.texture.subsurface(pygame.Rect(898, 110, 75, 75))
             self.current_speed = 0
+
 
         self.rect.move_ip((self.current_speed, 0))
 
@@ -98,7 +161,7 @@ class Car(pygame.sprite.Sprite):
 
         image_name = "assets/car{}.png".format(random.randint(1, 4))
 
-        self.image = pygame.image.load(image_name)
+        self.image = pygame.image.load(image_name).subsurface(pygame.Rect(6, 3, 68, 111))
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = (p[random.randint(0, 2)], 0)
